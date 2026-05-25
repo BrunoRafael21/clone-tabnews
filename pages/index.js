c00b95ecdc7a492a0366c894e91b877427e81ea9
Bruno Rafael <profissional.bruno.rafael@gmail.com>
2026-05-25 09:03:41 -0300

    Adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Raquel, amo você!! Se me ama, dê uma risadinha!</h1>
-}           
+  return <h1>Raquel, amo você!! Se me ama, dê uma risadinha!</h1>;
+}
 
 export default Home;
